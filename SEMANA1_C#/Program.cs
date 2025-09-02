98ee0fe741ecc562115286e2de7b052fa7e48c7b
FRANK <FRANKQUISPE063@GMAIL.COM>
2025-09-02 15:31:09 -0500

    subiendo ejercicio1-SEMAN1

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+            Console.WriteLine("INGRESA TU NOMBRE: ");
+            nombre = (Console.ReadLine());
+            Console.WriteLine("INGRESA EL NOMBRE DE TU CARRERA: ");
+            carrera = (Console.ReadLine());
+            //MENSAJE DE BIENVENIDAD
+            Console.WriteLine($"\n{nombre}, BIENVENIDOA FUND.ALGORIT{carrera}");
+        }
+        static void ejer1() 
+        {
+        }
+        static void ejer2()
+        {
+        }
+        static void ejer3()
+        {
+        }
+        static void ejer4()
+        {
+        }
+    }
+}
---
+++ SEMANA_PYTHON/SEMANA_PYTHON.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre = input("INGRESE SU NOMBRE")
+    carrera = input("INGRESE SU CARRERA")
+    
+    print(f"{nombre}, BIENVENIDO A FUND.ALGORIT {carrera}")
+
+ejer1()
