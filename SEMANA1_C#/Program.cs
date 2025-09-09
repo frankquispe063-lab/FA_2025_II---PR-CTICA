d4a5c8853da685fbd414f4a4c35a591244a7151c
FRANK <FRANKQUISPE063@GMAIL.COM>
2025-09-09 15:38:03 -0500

    ejer5 sem3

--- SEMANA1_C#/Program.cs
+++ SEMANA1_C#/Program.cs
@@ -21,6 +21,10 @@ namespace SEMANA1_C_
         }
         static void ejer2() 
         {
+            Console.WriteLine("\"FRANK\"");
+        }
+        static void ejer3()
+        {
             Console.WriteLine("INGRESA NUMERO X: ");
             int X = int.Parse(Console.ReadLine());
 
@@ -32,16 +36,33 @@ namespace SEMANA1_C_
             Console.WriteLine("RESTA: " + (X - Y));
             Console.WriteLine("MULTIPLICACIÓN: " + (X * Y));
             Console.WriteLine("DIVISIÓN: " + resu);
-
-        }
-        static void ejer3()
-        {
         }
         static void ejer4()
         {
+            Console.Write("Ingrese un número decimal: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            double raiz2 = Math.Sqrt(num);
+            int redo = (int)Math.Round(num, 0);
+            double cubo = Math.Pow(num, 3);
+            double raiz3 = Math.Pow(num, 1 / 3d);
+
+            Console.WriteLine("Raiz 2: " + raiz2);
+            Console.WriteLine("Redondeado: " + redo);
+            Console.WriteLine("Al cubo: " + cubo);
+            Console.WriteLine("Raiz 3: " + raiz3);
         }
         static void ejer5()
         {
+            Console.Write("Ingrese número: ");
+            string num = (Console.ReadLine());
+
+            int entero = int.Parse(num);
+            double deci = double.Parse(num);
+
+            Console.WriteLine("Resto: " + (entero % 2));
+            Console.WriteLine("División: "+ (deci))
+
         }
     }
 }
--- SEMANA_PYTHON/SEMANA_PYTHON.py
+++ SEMANA_PYTHON/SEMANA_PYTHON.py
@@ -5,6 +5,9 @@ def ejer1():
     print(f"{nombre}, BIENVENIDO A FUND.ALGORIT {carrera}")
 
 def ejer2():
+    print ("\"FRANK\"")
+
+def ejer3():
     x= int(input("Ingresa el valor de x: "))
     y= int(input("Ingresa el valor de y: "))
 
@@ -13,4 +16,23 @@ def ejer2():
     print ("multiplicación: ", (x*y))
     print ("división: ", (x/y))
 
-ejer2()
+    import math #importando la libreria math
+
+def ejer4():
+    num = float(input("Ingrese un número decimal:"))
+
+    print ("Raaiz 2: ", math.sqrt(num))
+    print ("Redondeado", round(num, 0))
+    print ("al cubo: ", math.pow(num, 3))
+    print ("Raaiz 3: ", num ** (1/3))
+
+def ejer5():
+    num =  input  ("Ingrese número: ")
+
+    entero = int(num)
+    deci = float(num)
+
+    print ("Resto: ", (entero%2))
+    print ("División: ", (deci/3))
+
+ejer5()
